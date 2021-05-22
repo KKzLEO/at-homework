fa622cb660c4eef37b72e12ebec71ea364b12de6
Li, YinJun | Leo | TWR <yinjun.li@rakuten.com>
2021-05-23 00:32:50 +0800

    feat(app): add `<Calendar>` component to app for demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Calendar from 'components/Calendar'
+import { inspect } from '@xstate/inspect'
+import { getUrlParams } from 'utils/base'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+if (getUrlParams().debug) {
+  inspect({
+    iframe: false,
+  })
 }
 
-export default App;
+function App() {
+  return <Calendar />
+}
+
+export default App
