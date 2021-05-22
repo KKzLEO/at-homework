3e60b5f5e1bd65f195fde37489ac64a291c8e7df
Li, YinJun | Leo | TWR <yinjun.li@rakuten.com>
2021-05-23 00:31:04 +0800

    feat(constants): add constants for breakpoint and calendar

---
+++ src/constants/breakpoints.js
@@ -0,0 +1,8 @@
+const breakpoints = {
+  xLarge: '1200px',
+  large: '992px',
+  medium: '768px',
+  small: '576px',
+}
+
+export default breakpoints
---
+++ src/constants/calendar.js
@@ -0,0 +1,6 @@
+export const MAX_YEAR = 2100
+
+export const STATUSES = {
+  BOOKED: 'BOOKED',
+  AVAILABLE: 'AVAILABLE',
+}
