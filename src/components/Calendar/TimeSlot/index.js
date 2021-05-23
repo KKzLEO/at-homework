9d3d6da50e7c8271016ac2bc84110a4fb2c176e8
Li, YinJun | Leo | TWR <yinjun.li@rakuten.com>
2021-05-24 03:02:49 +0800

    fix(calendar): wrong intervals

--- src/components/Calendar/TimeSlot/index.js
+++ src/components/Calendar/TimeSlot/index.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import * as Style from '../style'
-import { format } from 'utils/date'
+import { format, add } from 'utils/date'
 import { STATUSES } from 'constants/calendar'
 
 const TimeSlot = ({ start, end, status }) => {
+  const halfHours = (Math.abs(end - start) / 36e5) * 2
+
+  if (halfHours <= 0) return null
+
+  const timeSlots = Array(halfHours)
+    .fill(0)
+    .map((_, index) => {
+      return add(start, { hours: 0.5 * index })
+    })
+
   return (
-    <div key={+new Date(start)}>
-      <Style.Time isAvailable={status === STATUSES.AVAILABLE}>
-        {format(start, 'HH:mm')}
-      </Style.Time>
-    </div>
+    <>
+      {timeSlots.map((date) => (
+        <div key={+date}>
+          <Style.Time isAvailable={status === STATUSES.AVAILABLE}>
+            {format(date, 'HH:mm')}
+          </Style.Time>
+        </div>
+      ))}
+    </>
   )
 }
 
